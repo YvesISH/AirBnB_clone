81f7127b29fca90cddf6b080594d6110bf15e2ce
YvesISH <ivanmutware999@gmail.com>
2023-02-09 00:41:43 -0800

    Amenity

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module amenity inherits BaseModel"""
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Public class attributes"""
+
+    name = ""
