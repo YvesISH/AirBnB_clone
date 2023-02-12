65058ba08add67e23f594df74ff7236664ee88bc
YvesISH <ivanmutware999@gmail.com>
2023-02-12 12:38:53 -0800

    AirBnB

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Init file for models package"""
 
-from models.engine import file_storage
+from models.engine.file_storage import FileStorage
 
-storage = file_storage.FileStorage()
+storage = FileStorage()
 storage.reload()
